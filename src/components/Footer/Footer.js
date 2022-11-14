d56d980ecc0c2429b9809e62ac2c5e09c76b6f75
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2022-11-14 05:56:11 +0530

    Spotify Footer Component Updated :)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -128,44 +128,6 @@ const Footer = (props) => {
         </Grid>
       </div>
     </div>
-
-    // <div className="footer">
-    //   <div className="footer__left">
-    //     <img
-    //       className="footer__albumLogo"
-    //       src=""
-    //       alt={item?.name}
-    //     />
-    //     <div className="footer__songInfo">
-    //       <h4>Yeah!</h4>
-    //       <p>User</p>
-    //     </div>
-    //   </div>
-
-    //   <div className="footer__center">
-    //     <ShuffleIcon className="footer__green" />
-    //     <SkipPreviousIcon className="footer__icon" />
-    //     <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
-    //     <SkipNextIcon className="footer__icon" />
-    //     <RepeatIcon className="footer__green" />
-    //   </div>
-
-    //   <div className="footer__right">
-    //     <Grid container spacing={2}>
-    //       <Grid item>
-    //         <PlaylistPlayIcon />
-    //       </Grid>
-
-    //       <Grid item>
-    //         <VolumeDownIcon />
-    //       </Grid>
-
-    //       <Grid item xs>
-    //         <Slider />
-    //       </Grid>
-    //     </Grid>
-    //   </div>
-    // </div>
   );
 };
 
